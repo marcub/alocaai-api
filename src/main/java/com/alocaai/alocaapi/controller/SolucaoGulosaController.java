1edf3a4b9e903c9f598a1f38fb6859e831d7e323
Marcus Cezar <42722469+marcub@users.noreply.github.com>
2024-04-24 00:29:50 -0300

    Ajustando permissão cors para front

--- src/main/java/com/alocaai/alocaapi/controller/SolucaoGulosaController.java
+++ src/main/java/com/alocaai/alocaapi/controller/SolucaoGulosaController.java
@@ -6,21 +6,19 @@ import com.alocaai.alocaapi.service.SolucaoGulosaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SolucaoGulosaController {
 
     @Autowired
     private SolucaoGulosaService solucaoGulosaService;
 
-    @GetMapping("/solucaoGulosa")
+    @PostMapping("/solucaoGulosa")
     public ResponseEntity<List<Turma>> geraSolucao(@RequestBody CreateSolucaoDTO createSolucaoDTO) {
 
         List<Turma> turmasAlocadas = solucaoGulosaService.createSolucao(createSolucaoDTO);
